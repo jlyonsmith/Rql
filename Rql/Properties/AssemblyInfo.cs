75d48ffbf42031a2673514bdd04f4a0175b991a4
jlyonsmith <john@lyon-smith.org>
2013-08-05 17:41:59 -0700

    Update assembly info.

--- Rql/Properties/AssemblyInfo.cs
+++ Rql/Properties/AssemblyInfo.cs
@@ -1,22 +1,19 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
-[assembly: AssemblyTitle("Rql")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("john")]
+[assembly: AssemblyTitle("RQL")]
+[assembly: AssemblyDescription("REST Query Language")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif 
+[assembly: AssemblyCompany("John Lyon-Smith")]
+[assembly: AssemblyProduct("RQL")]
+[assembly: AssemblyCopyright("(c) 2013, John Lyon-Smith.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.0.*")]
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.10805.5")]
+
 
